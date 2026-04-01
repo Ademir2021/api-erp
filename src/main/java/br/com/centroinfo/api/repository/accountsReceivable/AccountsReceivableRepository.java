ef272cb8e18a21c1bda1e842db7f1db20b35c036
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-01 15:57:35 -0300

    save codes

--- src/main/java/br/com/centroinfo/api/repository/accountsReceivable/AccountsReceivableRepository.java
+++ src/main/java/br/com/centroinfo/api/repository/accountsReceivable/AccountsReceivableRepository.java
@@ -10,41 +10,21 @@ import br.com.centroinfo.api.entities.accountsReceivable.AccountsReceivable;
 
 public interface AccountsReceivableRepository extends JpaRepository<AccountsReceivable, Long> {
 
-    String ACCOUNTS_RECEIVALBE_RESPONSE =("""
-    SELECT new br.com.centroinfo.api.dtos.accountsReceivableDTO.AccountsReceivableResponseDTO(
-        ar.id,
-        ar.createdAt,
-        ar.updatedAt,
-        b.id,
-        b.name,
-        u.id,
-        u.login,
-        p.id,
-        p.name,
-        s.id,
-        ar.value,
-        ar.receivedValue,
-        ar.balance,
-        ar.dueDate,
-        ar.description,
-        ar.situation,
-        ar.observations,
-        ar.lateFee,
-        ar.interest,
-        ar.discount,
-        ar.type,
-        ar.idTypeOperation,
-        ar.descriptionTypeOperation
-    )
-    FROM AccountsReceivable ar
-    LEFT JOIN ar.branch b
-    LEFT JOIN ar.user u
-    LEFT JOIN ar.payer p
-    LEFT JOIN ar.sale s
-""");
-  
+    String ACCOUNTS_RECEIVABLE_RESPONSE = ("""
+                SELECT new br.com.centroinfo.api.dtos.accountsReceivableDTO.AccountsReceivableResponseDTO(
+                    ar.id, ar.createdAt, ar.updatedAt,
+                    b.id, b.name, u.id, u.login, p.id, p.name, s.id,
+                    ar.value, ar.receivedValue, ar.balance, ar.dueDate, ar.description, ar.situation,
+                    ar.observations, ar.lateFee, ar.interest, ar.discount, ar.type,
+                    ar.idTypeOperation, ar.descriptionTypeOperation )
+                FROM AccountsReceivable ar
+                LEFT JOIN ar.branch b
+                LEFT JOIN ar.user u
+                LEFT JOIN ar.payer p
+                LEFT JOIN ar.sale s
+            """);
 
-    @Query(ACCOUNTS_RECEIVALBE_RESPONSE)
+    @Query(ACCOUNTS_RECEIVABLE_RESPONSE)
     List<AccountsReceivableResponseDTO> findAccountsReceivable();
 
 }
