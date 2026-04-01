95d3f999a5d349d29ab94acb27072f81c801a8a8
Ademir Souza de Almeida <centroserra@gmail.com>
2026-03-31 23:34:15 -0300

    save codes

--- src/main/java/br/com/centroinfo/api/services/sale/SaleService.java
+++ src/main/java/br/com/centroinfo/api/services/sale/SaleService.java
@@ -1,12 +1,13 @@
 package br.com.centroinfo.api.services.sale;
 
-
 import br.com.centroinfo.api.dtos.saleDTO.ItemSaleDTO;
 import br.com.centroinfo.api.dtos.saleDTO.SaleDTO;
 import br.com.centroinfo.api.entities.accountsReceivable.AccountsReceivable;
 import br.com.centroinfo.api.entities.sales.ItemSale;
 import br.com.centroinfo.api.entities.sales.Sale;
 import br.com.centroinfo.api.repository.sale.SaleRepository;
+
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -46,7 +47,6 @@ public class SaleService {
         }
 
         List<AccountsReceivable> accountsReceivableList = new ArrayList<>();
-
         Optional.ofNullable(saleDTO.getAccountsReceivable())
                 .orElse(Collections.emptyList())
                 .forEach(accountsReceivableDTO -> {
@@ -69,7 +69,6 @@ public class SaleService {
                     accountsReceivable.setBalance(accountsReceivableDTO.getValue());
                     accountsReceivable.setReceivedValue(accountsReceivableDTO.getReceivedValue());
                     accountsReceivable.setCreatedAt(LocalDateTime.now());
-
                     accountsReceivableList.add(accountsReceivable);
                 });
 
