30133ea0d3819bb10556ec93b28a2f51606dfe40
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-16 13:32:41 -0300

    save codes

---
+++ src/main/resources/db/migration/V2__insert-table-type_items.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO type_items values(1, '00 - Mercadoria para Revenda');
+UPDATE type_items SET name = '01 - Mercadoria para Revenda' where id = 1
+INSERT INTO type_items values(2, '02 - Produto');
+INSERT INTO type_items values(3, '03 - Servico');
+INSERT INTO type_items values(2, '04 - Consumo');
+INSERT INTO type_items values(2, '05 - Imobilizado');
