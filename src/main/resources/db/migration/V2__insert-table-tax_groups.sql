575a5c133d000cc21f8ae2602caf92eb247718b4
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-16 13:23:31 -0300

    save codes

---
+++ src/main/resources/db/migration/V2__insert-table-tax_groups.sql
@@ -0,0 +1,4 @@
+
+insert into tax_groups values(1, 'Mercadoria Tributada Normalmente', 1)
+insert into tax_groups values(2, 'Mercadoria Trib. por Substituição Tributária', 2);
+insert into tax_groups values(3, 'Serviço Tributado pelo ISS', 3)
