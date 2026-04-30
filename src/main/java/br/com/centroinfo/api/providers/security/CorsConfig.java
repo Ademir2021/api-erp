aad38deb3ced3f744bf8073f4180fa8029ceb726
Ademir Souza de Almeida <centroserra@gmail.com>
2026-04-30 10:57:53 -0300

    save codes

--- src/main/java/br/com/centroinfo/api/providers/security/CorsConfig.java
+++ src/main/java/br/com/centroinfo/api/providers/security/CorsConfig.java
@@ -14,14 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // ou apenas "/auth/**"
-                        .allowedOrigins("http://localhost:3000") // frontend dev
-                        // .allowedOrigins("http://192.168.80.101:3000") // frontend dev
-                        .allowedOriginPatterns("http://192.168.80.*:3000")
-                        // .allowedOrigins("http://localhost:4173") // frontend build
+                        .allowedOriginPatterns(
+                                "http://localhost:3000",
+                                "http://192.168.80.*:3000") // frontend dev
+                        .allowCredentials(true)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
+                        .allowedHeaders("*");
                         // .allowedHeaders("Authorization", "Content-Type");
-                        .allowCredentials(true);
             }
         };
     }
