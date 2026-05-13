453352e520a84e1b3aee2ab4153e4129dfec8539
Ademir Souza de Almeida <centroserra@gmail.com>
2026-05-13 16:04:52 -0300

    save codes

--- src/main/java/br/com/centroinfo/api/services/sale/NotaCupomService.java
+++ src/main/java/br/com/centroinfo/api/services/sale/NotaCupomService.java
@@ -5,8 +5,6 @@ import java.text.NumberFormat;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-
-
 import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +21,7 @@ import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Rectangle;
 import com.lowagie.text.pdf.PdfWriter;
+
 import java.awt.image.BufferedImage;
 
 import br.com.centroinfo.api.entities.sales.ItemSale;
@@ -31,7 +30,7 @@ import br.com.centroinfo.api.entities.sales.Sale;
 @Service
 public class NotaCupomService {
 
-        private Image gerarQRCode(String text, int width, int height) {
+    private Image gerarQRCode(String text, int width, int height) {
         try {
             QRCodeWriter writer = new QRCodeWriter();
             BitMatrix matrix = writer.encode(text, BarcodeFormat.QR_CODE, width, height);
@@ -44,109 +43,126 @@ public class NotaCupomService {
     }
 
     public byte[] gerarCupom(Sale sale) {
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
-    try {
-        // 📏 Tamanho tipo bobina 80mm
-        Rectangle pageSize = new Rectangle(226, 800);
-        Document document = new Document(pageSize, 10, 10, 10, 10);
+        int quantidadeItens = sale.getItemsSale().size();
+        float altura = 500f + (quantidadeItens * 26f);
+        float largura = 226f;
+        // System.out.println("Altura: " + altura);
 
-        PdfWriter.getInstance(document, outputStream);
-        document.open();
+        try {
+            // 📏 Tamanho tipo bobina 80mm
+            Rectangle pageSize = new Rectangle(largura, altura);
+            Document document = new Document(pageSize, 10, 10, 10, 10);
 
-        // 🔤 Fontes
-        Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10);
-        Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 9);
+            PdfWriter.getInstance(document, outputStream);
+            document.open();
 
-        NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+            // 🔤 Fontes
+            Font headerFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 10);
+            Font normalFont = FontFactory.getFont(FontFactory.HELVETICA, 9);
 
-        // ================= HEADER =================
-        Paragraph empresa = new Paragraph();
-        empresa.setAlignment(Element.ALIGN_CENTER);
+            NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
 
-        empresa.add(new Chunk(sale.getBranch().getFantasyName() + "\n", headerFont));
-        empresa.add(new Chunk("CNPJ: " + sale.getBranch().getCnpj() + "\n", normalFont));
-        empresa.add(new Chunk("Tel: " + sale.getBranch().getPhoneNumber() + "\n", normalFont));
-        empresa.add(new Chunk("----------------------------------\n"));
+            // ================ LINES ==================
+            String line = "---------------------------------------------";
 
-        document.add(empresa);
+            // ================= HEADER =================
+            Paragraph empresa = new Paragraph();
+            empresa.setAlignment(Element.ALIGN_CENTER);
 
-        // ================= VENDA =================
-        document.add(new Paragraph("CUPOM NÃO FISCAL", headerFont));
-        document.add(new Paragraph("Venda Nº: " + String.format("%06d", sale.getId()), normalFont));
-        document.add(new Paragraph(
-                "Data: " + sale.getIssueDate()
-                        .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
-                normalFont));
-
-        document.add(new Paragraph("----------------------------------"));
-
-        // ================= CLIENTE =================
-        if (sale.getPerson() != null) {
-            document.add(new Paragraph("Cliente: " + sale.getPerson().getName(), normalFont));
-
-            if (sale.getPerson().getCpf() != null) {
-                document.add(new Paragraph("CPF: " + sale.getPerson().getCpf(), normalFont));
-            }
-        }
-
-        document.add(new Paragraph("----------------------------------"));
-
-        // ================= ITENS =================
-        for (ItemSale item : sale.getItemsSale()) {
-            BigDecimal total = item.getAmount().multiply(item.getPrice());
-
-            document.add(new Paragraph(item.getItem().getName(), normalFont));
-
-            document.add(new Paragraph(
-                    item.getAmount() + " x " +
-                            nf.format(item.getPrice()) + " = " +
-                            nf.format(total),
+            empresa.add(new Chunk(sale.getBranch().getFantasyName() + "\n", headerFont));
+            empresa.add(new Chunk("CNPJ: " + sale.getBranch().getCnpj() + "\n", normalFont));
+            empresa.add(new Chunk("Tel: " + sale.getBranch().getPhoneNumber() + "\n", normalFont));
+            empresa.add(new Chunk(sale.getBranch().getPerson().getAddress().getStreet() + " "
+                    + sale.getBranch().getPerson().getAddress().getNumber() + " "
+                    + sale.getBranch().getPerson().getAddress().getNeighborhood() + "\n"
+                    + sale.getBranch().getPerson().getAddress().getZipCode().getCity().getName() + " "
+                    + sale.getBranch().getPerson().getAddress().getZipCode().getCity().getState().getAcronym() + "\n",
                     normalFont));
+            empresa.add(new Chunk(line + "\n"));
+            document.add(empresa);
+
+            // ================= VENDA =================
+            document.add(new Paragraph("CUPOM NÃO FISCAL", headerFont));
+            document.add(new Paragraph("Venda Nº: " + String.format("%06d", sale.getId()), normalFont));
+            document.add(new Paragraph(
+                    "Data: " + sale.getIssueDate()
+                            .format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")),
+                    normalFont));
+
+            document.add(new Paragraph(line));
+
+            // ================= CLIENTE =================
+            if (sale.getPerson() != null) {
+                document.add(new Paragraph("Cliente: " + sale.getPerson().getName(), normalFont));
+
+                if (sale.getPerson().getCpf() != null) {
+                    document.add(new Paragraph("CPF: " + sale.getPerson().getCpf(), normalFont));
+                }
+            }
+
+            document.add(new Paragraph(line));
+
+            // ================= ITENS =================
+            for (ItemSale item : sale.getItemsSale()) {
+                BigDecimal total = item.getAmount().multiply(item.getPrice());
+                document.add(new Paragraph(item.getItem().getName(), normalFont));
+                document.add(new Paragraph(
+                        item.getAmount() + " x " +
+                                nf.format(item.getPrice()) + " = " +
+                                nf.format(total),
+                        normalFont));
+            }
+
+            document.add(new Paragraph(line));
+
+            // ================= TOTAIS =================
+            document.add(new Paragraph("Desconto: " + nf.format(sale.getDiscount()), normalFont));
+
+            Paragraph total = new Paragraph("TOTAL: " + nf.format(sale.getTotalNote()), headerFont);
+            total.setAlignment(Element.ALIGN_RIGHT);
+            document.add(total);
+
+            // ================= PAGAMENTO =================
+            // String IDPay = sale.getAccountsReceivable().get(0).getIdTypeOperation();
+
+            String IDPay = "";
+
+            if (sale.getAccountsReceivable() != null &&
+                    !sale.getAccountsReceivable().isEmpty() &&
+                    sale.getAccountsReceivable().get(0) != null) {
+                IDPay = sale.getAccountsReceivable().get(0).getIdTypeOperation();
+            }
+            String tipoPagamento = sale.getOperationSale().getId() == 3 ? "FATURA: " + IDPay
+                    : sale.getOperationSale().getId() == 2 ? "CARTÃO - ID:" + IDPay : "PIX: " + IDPay;
+            document.add(new Paragraph("\nPagamento: " + tipoPagamento, normalFont));
+            document.add(new Paragraph(line));
+
+            // ================= QR CODE =================
+            Image qr = gerarQRCode("http://localhost/cupom/" + sale.getId() + "/pdf", 80, 80);
+            if (qr != null) {
+                qr.setAlignment(Element.ALIGN_CENTER);
+                document.add(qr);
+            }
+
+            document.add(new Paragraph("\n" + line));
+
+            // ================= RODAPÉ =================
+            Paragraph rodape = new Paragraph(
+                    "CUPOM NÃO FISCAL\nObrigado pela preferência!",
+                    normalFont);
+
+            rodape.setAlignment(Element.ALIGN_CENTER);
+            document.add(rodape);
+            document.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Erro: " + e);
         }
 
-        document.add(new Paragraph("----------------------------------"));
-
-        // ================= TOTAIS =================
-        document.add(new Paragraph("Desconto: " + nf.format(sale.getDiscount()), normalFont));
-
-        Paragraph total = new Paragraph("TOTAL: " + nf.format(sale.getTotalNote()), headerFont);
-        total.setAlignment(Element.ALIGN_RIGHT);
-        document.add(total);
-
-        document.add(new Paragraph("----------------------------------"));
-
-        // ================= PAGAMENTO =================
-        String tipoPagamento =
-                sale.getOperationSale().getId() == 3 ? "FATURA" :
-                sale.getOperationSale().getId() == 2 ? "CARTÃO" : "PIX";
-
-        document.add(new Paragraph("Pagamento: " + tipoPagamento, normalFont));
-
-        // ================= QR CODE =================
-        Image qr = gerarQRCode("http://localhost/cupom/" + sale.getId() + "/pdf", 80, 80);
-        if (qr != null) {
-            qr.setAlignment(Element.ALIGN_CENTER);
-            document.add(qr);
-        }
-
-        document.add(new Paragraph("----------------------------------"));
-
-        // ================= RODAPÉ =================
-        Paragraph rodape = new Paragraph(
-                "CUPOM NÃO FISCAL\nObrigado pela preferência!",
-                normalFont);
-
-        rodape.setAlignment(Element.ALIGN_CENTER);
-        document.add(rodape);
-
-        document.close();
-
-    } catch (Exception e) {
-        e.printStackTrace();
+        return outputStream.toByteArray();
     }
 
-    return outputStream.toByteArray();
-}
-    
 }
