2ffab4560828291455a0ec7f82ad06b43397d94f
Ademir Souza de Almeida <centroserra@gmail.com>
2026-09-23 20:19:36 -0300

    save codes

--- src/main/java/br/com/centroinfo/api/services/itemsImagesService/ItemImageService.java
+++ src/main/java/br/com/centroinfo/api/services/itemsImagesService/ItemImageService.java
@@ -2,9 +2,11 @@ package br.com.centroinfo.api.services.itemsImagesService;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,30 +51,92 @@ public class ItemImageService {
                 .toList();
     };
 
+    // public void updateImages(
+    // Item item,
+    // List<MultipartFile> images) {
+    // if (images == null || images.isEmpty()) {
+    // return;
+    // }
+    // for (MultipartFile file : images) {
+    // if (file == null || file.isEmpty()) {
+    // continue;
+    // }
+    // try {
+    // String originalFileName = file.getOriginalFilename();
+    // Path directory = Paths.get("imgs", "items");
+    // Files.createDirectories(directory);
+    // ItemsImages image = new ItemsImages();
+    // image.setFileName(originalFileName);
+    // image.setFilePath("/imgs/items/");
+    // image.setItem(item);
+    // itemsImagesRepository.save(image);
+    // } catch (IOException e) {
+    // throw new RuntimeException(
+    // "Erro ao salvar imagem do item", e);
+    // }
+    // }
+    // }
+
     public void updateImages(
             Item item,
             List<MultipartFile> images) {
+
         if (images == null || images.isEmpty()) {
             return;
         }
 
-        for (MultipartFile file : images) {
-            if (file == null || file.isEmpty()) {
-                continue;
-            }
-            try {
+        try {
+            Path directory = Paths.get("imgs", 
+            "items", 
+            String.valueOf(item.getId()));
+
+            // Cria a pasta caso não exista
+            Files.createDirectories(directory);
+
+            for (MultipartFile file : images) {
+
+                if (file == null || file.isEmpty()) {
+                    continue;
+                }
+
+                // Nome original
                 String originalFileName = file.getOriginalFilename();
-                Path directory = Paths.get("imgs", "items");
-                Files.createDirectories(directory);
+
+                // Recupera somente a extensão
+                String extension = "";
+
+                if (originalFileName != null) {
+                    int index = originalFileName.lastIndexOf(".");
+                    if (index > 0) {
+                        extension = originalFileName.substring(index).toLowerCase();
+                    }
+                }
+
+                // Gera novo nome
+                String uuidFileName = UUID.randomUUID() + extension;
+
+                // Caminho físico
+                Path filePath = directory.resolve(uuidFileName);
+
+                // Salva a imagem no disco
+                Files.copy(
+                        file.getInputStream(),
+                        filePath,
+                        StandardCopyOption.REPLACE_EXISTING);
+
+                // Salva informações no banco
                 ItemsImages image = new ItemsImages();
-                image.setFileName(originalFileName);
-                image.setFilePath("/imgs/items/");
+
+                image.setFileName(uuidFileName);
+                image.setFilePath("/imgs/items/" + uuidFileName);
                 image.setItem(item);
+
                 itemsImagesRepository.save(image);
-            } catch (IOException e) {
-                throw new RuntimeException(
-                        "Erro ao salvar imagem do item", e);
             }
+
+        } catch (IOException e) {
+            throw new RuntimeException(
+                    "Erro ao salvar imagem do item", e);
         }
     }
 
